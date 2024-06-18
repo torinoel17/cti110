c61499e40d8429e6333635f1695e6eb6f1e455d8
torinoel17 <stricklt2720@student.faytechcc.edu>
2024-06-17 21:42:13 -0400

    First submission.

---
+++ P1LAB1_StricklandTori.py
@@ -0,0 +1,10 @@
+# This is a comment.
+print("Tori Strickland")
+print("06/15/2024")
+print ("PLAB1")
+print ("Creating a program.")
+# Get user input for first name and last name
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+# Display a welcome message
+print("Hello user! Welcome to CTI-110")
